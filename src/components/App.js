59a4ce0881c27ea248c9706420c16359acb2e896
BharatJivvTate <hibharatj@gmail.com>
2025-07-26 13:17:35 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
+import React from 'react';
+import Tooltip from './Tooltip';
 
-import React from "react";
-import './../styles/App.css';
-
-const App = () => {
+function App() {
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div style={{ padding: '50px' }}>
+      <h1>React Tooltip Demo</h1>
+      
+      <Tooltip text="This is a tooltip for the button!">
+        <button>Hover over me</button>
+      </Tooltip>
+
+      <br /><br />
+
+      <Tooltip text="This is some tooltip text for a paragraph.">
+        <p>Hover over this paragraph to see a tooltip.</p>
+      </Tooltip>
+
+      <br /><br />
+
+      <Tooltip text="Even icons can have tooltips!">
+        <span role="img" aria-label="info">ℹ️</span>
+      </Tooltip>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
