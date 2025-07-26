c683c3e1bbe8d95b2c82daac104e75ec70758579
BharatJivvTate <hibharatj@gmail.com>
2025-07-26 14:21:14 +0530

    fix h2

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,13 @@ function App() {
   return (
     <div style={{ padding: "50px" }}>
       <h2>React Tooltip Demo</h2>
+      
       <h2 className="tooltip">
+        <div>
         <Tooltip text="This is a tooltip for the button!">
-          <div>Hover over me</div>
+          Hover over me
         </Tooltip>
+        </div>
       </h2>
       <br />
       <br />
@@ -23,9 +26,9 @@ function App() {
       <br />
       <br />
 
-      <h2 className="tooltip">
+      {/* <h2 className="tooltip">
         <Tooltip text="Even icons can have tooltips!">Lorem2</Tooltip>
-      </h2>
+      </h2> */}
     </div>
   );
 }
